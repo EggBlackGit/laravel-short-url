601e413614a1b12062abbfdd7c897edcb97ca431
sutthiphongGit <85241992+EggBlackGit@users.noreply.github.com>
2025-05-14 18:44:45 +0700

    Update IsAdmin.php
    update route

--- app/Http/Middleware/IsAdmin.php
+++ app/Http/Middleware/IsAdmin.php
@@ -19,6 +19,6 @@ class IsAdmin
         if(auth()->user()->is_admin == 1){
             return $next($request);
         }
-        return redirect('home')->with('error', "don't is admin");
+        return redirect(route('admin.home'))->with('error', "don't is admin");
     }
 }
